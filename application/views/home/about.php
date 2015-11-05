7b07fb19a49642e6decb5153ce339b3603dd6421
superzhang <334198295@qq.com>
2015-11-05 11:07:29 +0800

    about 页修改

---
+++ application/views/home/about.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>关于</title>
+</head>
+<style>
+#main {
+    position: absolute;
+    width:900px;
+    height:200px;
+    left:30%;
+    top:50%;
+    margin-left:-100px;
+    margin-top:-100px;
+
+}
+</style>
+<body background="/public/img/about.jpg" style="width:100%">
+
+<div id="main"><font color="white" size="14">1000Auction,是基于一群小伙伴的兴趣，开发的拍卖信息资讯平台</font></div>
+</body>
+</html>
